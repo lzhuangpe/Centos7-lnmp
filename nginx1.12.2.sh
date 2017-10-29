362f355424145d0d2c23ab288b04cefa01d2ee98
huangpe <ahuangpe@163.com>
2017-10-29 15:45:22 +0800

    2017-10-29 15:45

--- nginx1.12.2.sh
+++ nginx1.12.2.sh
@@ -32,7 +32,7 @@ cd $nginx_ver
     --conf-path=$nginx_location/etc/nginx.conf \
     --user=$user_name --group=$user_name \
     --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock \
-    --error-log-path=$nginx_location/log/error.log --http-log-path=$nginx_location\log\access.log \
+    --error-log-path=$nginx_location/log/error.log --http-log-path=$nginx_location/log/access.log \
     --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp \
     --with-http_ssl_module --with-http_realip_module --with-http_addition_module --with-http_sub_module \
     --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module \
@@ -49,5 +49,6 @@ make install
 #配置开机启动
 \cp ../init.d/init.d.nginx /etc/init.d/nginx
 chmod +x /etc/init.d/nginx
+chkconfig nginx on
 /etc/init.d/nginx start
 
