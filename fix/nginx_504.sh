206a52db06c06412cecd242860c91b40a0c2fb33
huangpe <ahuangpe@163.com>
2017-12-14 17:32:59 +0800

    2017-12-14 17:32

--- fix/nginx_504.sh
+++ fix/nginx_504.sh
@@ -8,7 +8,7 @@ nginx_conf=`find /usr/local/nginx/etc/conf.d -type f -name "*.conf"`
 
 sed -i 's/max_execution_time = 30/max_execution_time = 300/g' $php_ini
 sed -i 's/;request_terminate_timeout = 0/request_terminate_timeout = 300/g' $php_fpm_conf
-sed -i '/^.*fastcgi_param.*SCRIPT_FILENAME.*$/a\\tfastcgi_read_timeout 300;' $nginx_conf
+sed -i '/^.*fastcgi_param.*SCRIPT_FILENAME.*$/a\\t fastcgi_read_timeout 300;' $nginx_conf
 
 grep "max_execution_time" $php_ini
 grep "request_terminate_timeout" $php_fpm_conf
